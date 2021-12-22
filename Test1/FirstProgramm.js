594ca1f7147832353a43577e62fe12f7a755bf2d
Korv1N <vladimir.todorov.ukraine@gmail.com>
2021-12-22 10:05:51 +0200

    first commit

---
+++ Test1/FirstProgramm.js
@@ -0,0 +1,20 @@
+"use strict";
+
+
+let i = 4, incr = 10, decr = 10;
+
+incr++;
+decr--;
+
+console.log('arr' + " - object");
+console.log(4 + " - object");
+console.log(i++ + +"5");
+console.log(i);
+console.log(incr, decr);
+
+const isChecked = true,
+    isClose = true;
+      
+console.log(isChecked && isClose && i!=4);
+
+
