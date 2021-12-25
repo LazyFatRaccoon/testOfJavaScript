a61c4c948514ce64b8d8c7bd19993df0fcc5d684
Korv1N <vladimir.todorov.ukraine@gmail.com>
2021-12-25 07:56:27 +0200

    modals added

--- 38.Создаем_табы_в_новом_проекте/js/script.js
+++ 38.Создаем_табы_в_новом_проекте/js/script.js
@@ -84,4 +84,53 @@ window.addEventListener("DOMContentLoaded", () => {
   }
 
   setClock(".timer", deadline);
+  // ----- END TIMER --------------------------------------------------------------------
+  // Modal ---------------------------------------------------------------
+  const modalTrigger = document.querySelectorAll("[data-modal]"),
+    modal = document.querySelector(".modal"),
+    modalCloseBtn = document.querySelector("[data-close]");
+
+  function openModal() {
+    modal.classList.toggle("fade");
+    modal.classList.toggle("show");
+    document.body.style.overflow = "hidden";
+    clearInterval(modalTimerID);
+  }
+  function closeModal() {
+    modal.classList.toggle("fade");
+    modal.classList.toggle("show");
+    document.body.style.overflow = "";
+  }
+
+  modalTrigger.forEach((item) => {
+    item.addEventListener("click", openModal);
+  });
+
+  modalCloseBtn.addEventListener("click", closeModal);
+
+  modal.addEventListener("click", (event) => {
+    if (event.target === modal) {
+      closeModal();
+    }
+  });
+
+  document.addEventListener("keydown", (e) => {
+    if (e.code === "Escape" && modal.classList.contains("show")) {
+      closeModal();
+    }
+  });
+
+  const modalTimerID = setTimeout(openModal, 15000);
+
+  function showModalByScroll() {
+    if (
+      window.pageYOffset + document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight - 1
+    ) {
+      openModal();
+      window.removeEventListener("scroll", showModalByScroll);
+    }
+  }
+  window.addEventListener("scroll", showModalByScroll);
+  console.log(modalTrigger);
 });
